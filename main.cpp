d38daf3fed0a8d73f62caff99776bd783bb67986
emy953 <80955889+emy953@users.noreply.github.com>
2022-11-30 16:11:35 +0200

    Tema3

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
@@ -25,12 +26,12 @@ private:
 
 public:
     Product(string name, int id,Components *components):name(name),id(id),components(components){};
-    Product():name(""),id(0){};
+    Product():name(""),id(0),components(new Components(0,"")){};
 
     Product& operator=(const Product& rhs)
     {
         Components *pOrig = components; // remember original pb
-        components = new Components(*rhs.components); // point pb to a copy of rhs’s bitmap
+        components = new Components(*rhs.components); // point pb to a copy of rhsâ€™s bitmap
         delete pOrig;
         name=rhs.name;
         id=rhs.id;
@@ -51,7 +52,9 @@ private:
     Factory& operator=(const Factory&) = delete;
 
 public:
-    Factory(string name,string founders,string address,int nr, Product* products):name(name),founders(founders),address(address),nr(nr),products(products){};
+    bool closed;
+    Factory(string name,string founders,string address,int nr, Product* products):name(name),founders(founders),address(address),closed(0),nr(nr),products(products){};
+    Factory():name("nume"),founders("founders"),address("address"),nr(0),closed(0),products(nullptr){};
     friend ostream& operator<<(ostream& os, const Factory& f);
 };
 
@@ -69,8 +72,40 @@ ostream& operator<<(ostream& os, const Factory& f)
             return os;
     }
 
+Factory* createDefaultFactory() //Functie care creeaza nou obiect alocat dinamic
+{
+    return (new Factory());
+}
+
+void closeFactory(Factory* f) //inchide o fabrica
+{
+    f->closed=1;
+}
+
+void openFactory(Factory* f) //deschide o facbrica
+{
+    f->closed=0;
+}
+
+class Close{  //Clasa RAII pentru inchiderea si deschiderea unei fabrici
+public:
+    explicit Close(Factory *f):fP(f)
+    {
+        closeFactory(fP);
+    }
+    ~Close()
+    {
+        openFactory(fP);
+    }
+private:
+    Factory *fP;
+    Close(const Close&) = delete;
+    Close& operator=(const Close&) = delete;
+};
+
 int main()
 {
+    /*
     int nr,id;
     Components *c=new Components(3,"c1 c2 c3");
 
@@ -84,9 +119,33 @@ int main()
             products[i]=p;
         }
         Factory f(namef,founder,address,nr,products);
-        cout<<f;
+        cout<<f;*/
     //Factory f1(f);
     //Factory f2("a","b",0,nullptr);
     //f2=f;
+
+
+    //Item 13
+
+    auto_ptr<Factory> p(createDefaultFactory());
+    auto_ptr<Factory> p2(p);
+    cout<<*p2;
+    //cout<<*p; Nu functioneaz pentru ca p e null dupa copiere.
+    shared_ptr<Factory> s(createDefaultFactory());
+    shared_ptr<Factory> s2(s);
+    cout<<*s2;
+    cout<<*s; //s si s2 sunt pointeri catre acelasi spatiu de memorie
+
+    //Item 14
+
+    Factory *f=new Factory();
+    cout<<f->closed<<endl; //open factory from constructor
+    Close close(f);        //close the factory
+    cout<<f->closed<<endl; //factory is closed
+    delete &close;         //object is deletede, open factory
+    cout<<f->closed<<endl; //factory is open
+    Close c(f);
+    cout<<f->closed<<endl; //factory is closed, will be open at the end of function main
+   // Close c2(c);  Compilation error, Close class is uncopyable
     return 0;
 }
